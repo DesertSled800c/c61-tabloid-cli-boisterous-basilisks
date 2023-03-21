8a3af542a2d8a1a464e236b3b1e37b87599b8bd5
Ben Hoberg <benjhoberg@gmail.com>
2023-03-21 13:28:43 -0500

    issue ticket #43 completed. user can now view a list of journal entries

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -11,6 +11,41 @@ namespace TabloidCLI
 		public JournalRepository(string connectionString) : base(connectionString)
 		{ }
 
+        public List<Journal> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id,
+                                               Title,
+                                               Content,
+                                               CreateDateTime
+                                          FROM Journal";
+
+                    List<Journal> journals = new List<Journal>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content= reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+
+                    return journals;
+                }
+            }
+        }
+
         public Journal Get(int id)
         {
             throw new NotImplementedException();
@@ -39,10 +74,6 @@ namespace TabloidCLI
             throw new NotImplementedException();
         }
 
-        public List<Journal> GetAll()
-        {
-            throw new NotImplementedException();
-        }
 
         public void Delete(int id)
         {

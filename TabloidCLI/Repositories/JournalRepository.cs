ff04aebee20806a69490283957f439d2f432b52c
Akshay Khona <DesertSled800c@gmail.com>
2023-03-21 11:38:28 -0500

    Aks Changes

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+	public class JournalRepository : DatabaseConnector, IRepository<Journal>
+	{
+		public JournalRepository(string connectionString) : base(connectionString)
+		{ }
+
+        public Journal Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Insert(Journal journal)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Journal (Title, Content)
+                                                     VALUES (@title, @content)";
+                    cmd.Parameters.AddWithValue("@title", journal.Title);
+                    cmd.Parameters.AddWithValue("@content", journal.Content);
+                    
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Journal entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Journal> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+	public class JournalManager : IUserInterfaceManager
+	{
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+		{
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) Add Journal");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Add();
+                    return this;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+        private void Add()
+        {
+            Console.WriteLine("New Journal");
+            Journal journal = new Journal();
+
+            Console.Write("New Title:");
+            journal.Title = Console.ReadLine();
+
+            Console.Write("Text Content:");
+            journal.Content = Console.ReadLine();
+
+            _journalRepository.Insert(journal);
+        }
+
+    }
+}
+

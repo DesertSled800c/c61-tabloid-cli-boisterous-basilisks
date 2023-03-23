93b6fb5b255be8fae746d62685f027ec9f765636
Morganero <morganero1@gmail.com>
2023-03-23 06:53:35 -0500

    changing branches to review pull request

--- TabloidCLI/Repositories/NoteRepository.cs
+++ TabloidCLI/Repositories/NoteRepository.cs
@@ -15,15 +15,18 @@ namespace TabloidCLI
     {
         public NoteRepository(string connectionString) : base(connectionString) { }
 
-        public List<Note> GetAll()
+        public List<Note> GetAllLinkedToPost(int id)
         {
             using (SqlConnection conn = new SqlConnection())
             {
                 conn.Open();
                 using (SqlCommand cmd = new SqlCommand())
                 {
-                    cmd.CommandText = @"SELECT Id, Title, Content, CreatDateTime
-                                       FROM Note";
+                    cmd.CommandText = @"SELECT n.Id, n.Title, n.Content, n.CreateDateTime
+                                        FROM Note n JOIN Post p ON n.postId = p.Id
+                                        WHERE p.Id = @id";
+                    cmd.Parameters.AddWithValue("id", id);
+
                     using(SqlDataReader reader = cmd.ExecuteReader())
                     {
                         List<Note> notes = new List<Note>();
@@ -45,18 +48,27 @@ namespace TabloidCLI
         }
 
 
-        public void Add(Note note)
+
+        /*for post detail manager
+         add private int _postId;
+        add it to contructor as well
+        // add choose method to postManager
+        // private AddNote() to postManager
+        //private RemoveNOte() to postManager
+        */
+
+        public void InsertNote(Post post, Note note) //add to PostRepo
         {
             using(SqlConnection conn= Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = new SqlCommand())
                 {
-                    cmd.CommandText = @"INSERT INTO Note(Title, Content, CreateDateTime.PostId)
-                                    VALUE(@title, @content, GETDATE(), @postId)";
+                    cmd.CommandText = @"INSERT INTO Note ( Title, Content, CreateDateTime, PostId)
+                                    VALUES (@title, @content, GETDATE(), @postId)";
                     cmd.Parameters.AddWithValue("@title", note.Title);
                     cmd.Parameters.AddWithValue("@content", note.Content);
-                    cmd.Parameters.AddWithValue("@postId", note.PostId);
+                    cmd.Parameters.AddWithValue("@postId", post.Id);
 
                     cmd.ExecuteNonQuery();
                 };
@@ -64,16 +76,17 @@ namespace TabloidCLI
 
         }
 
-        public void Remove(int id)
+        public void DeletNote(int postId, int id ) //Add to PostRepo
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = new SqlCommand())
                 {
-                    cmd.CommandText = @"DELETE FROM Note WHERE Id = @id";
+                    cmd.CommandText = @"DELETE FROM Note 
+                                        WHERE PostId = @postId AND Id = @id";
+                    cmd.Parameters.AddWithValue("@postId", postId);
                     cmd.Parameters.AddWithValue("@id", id);
-
                     cmd.ExecuteNonQuery();
                 }
             }
--- TabloidCLI/UserInterfaceManagers/NoteManager.cs
+++ TabloidCLI/UserInterfaceManagers/NoteManager.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using TabloidCLI.Repositories;
+using TabloidCLI.Models;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
@@ -13,20 +17,25 @@ namespace TabloidCLI.UserInterfaceManagers
         private readonly IUserInterfaceManager _parentUI;
         private NoteRepository _noteRepository;
         private string _connectionString;
+        private PostRepository _postRepository;
+        private int _postId;
 
-        public NoteManager(IUserInterfaceManager parentUI, string connectionString)
+        public NoteManager(IUserInterfaceManager parentUI, string connectionString, int postId)
         {
             _parentUI = parentUI;
             _noteRepository = new NoteRepository(connectionString);
-            _connectionString = connectionString;     
+            _postRepository = new PostRepository(connectionString);
+            _connectionString = connectionString;
+            _postId = postId;
+            
         }
 
-        public IUserInterfaceManager Exectute()
+        public IUserInterfaceManager Execute()
         {
             Console.WriteLine("Note Menu");
             Console.WriteLine(" 1) Add Note");
             Console.WriteLine(" 2) List Notes");
-            Console.WriteLine(" 3) Remove Note");
+            Console.WriteLine(" 3) Remove a Note");
             Console.WriteLine(" 0) Return");
 
 
@@ -34,16 +43,16 @@ namespace TabloidCLI.UserInterfaceManagers
             string choice = Console.ReadLine();
             switch (choice)
             {
-                case 1:
+                case "1":
                     Add();
                     return this;
-                case 2:
+                case "2":
                     List();
                     return this;
-                case 3:
+                case "3":
                     Remove();
                     return this;
-                case 4:
+                case "4":
                     return _parentUI;
                 default:
                     Console.WriteLine("Invalid Selection");
@@ -53,17 +62,57 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Add()
         {
+            Post post = _postRepository.Get(_postId);
+
+            Console.WriteLine("New Note");
+            Note note = new Note();
+
+            Console.WriteLine("What would you like to Title your Note?");
+            Console.Write("> ");
+            string titleEntered = Console.ReadLine();
+            note.Title = titleEntered;
+            Console.WriteLine();
+
+            Console.WriteLine("What content do you want to add to your Note?");
+            Console.Write("> ");
+            string contentEntered = Console.ReadLine();
+            note.Content = contentEntered;
+            
+
+            _noteRepository.InsertNote(post,note); //make this post repository
+            Console.WriteLine("Note Added");
+            Console.WriteLine();
+
 
         }
 
         private void List()
         {
+            Console.WriteLine("Your Current Notes");
+            List<Note> notes = _noteRepository.GetAllLinkedToPost(_postId);
 
+            foreach (Note note in notes)
+            {
+                int i = 0;
+                Console.WriteLine($"{i + 1}: {note.Title} \n\t{note.CreateDateTime}\n\t{note.Content}");
+            }
+            Console.WriteLine();
         }
 
         private void Remove()
         {
+            Console.WriteLine("Which Note would you like to remove from your post?");
+            List<Note> notes = _noteRepository.GetAllLinkedToPost( _postId);
 
+            for (int i = 0; i < notes.Count; i++)
+            {
+                Note note = notes[i];
+                Console.WriteLine($"{i + 1}: {note.Title}");
+            }
+            Console.Write("> ");
+            int deleteChoice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            _noteRepository.DeletNote(_postId, deleteChoice);
         }
     }
 }
